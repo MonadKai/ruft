6eae24c166b066dc90f7cd70b71f3e3941a6e010
MonadKai <onlookerliu_sjtu@163.com>
2020-11-22 20:11:29 +0800

    :new: Add project polars_examples

---
+++ prerequisite/polars_examples/src/main.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate polars;
+
+mod apply_closures;
+mod arithmetic;
+mod comparisons;
+mod groupby;
+mod iterators;
+mod joins;
+mod read_csv;
+mod temporal_data_types;
+
+fn main() {
+    println!("Hello, world!");
+}
